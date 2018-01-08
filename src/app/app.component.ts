cff8258eca4c1ca3a1708827207bdab9f906b5ec
Agnes Varga <Agnes Varga>
2018-01-08 12:20:43 +0100

    first commit with types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Types} from './Types';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Welcome to xyz';
+  types: Types[] ;
+
+  constructor(){
+    this.types = [
+              {'year': 2017,
+               'title':'Jin',
+                'keyword': 'emotion'},
+                {
+                  'year': 2018,
+                   'title':'Jang',
+                    'keyword': 'power',
+                }];
+  }
+
 }
---
+++ src/app/types.ts
@@ -0,0 +1,6 @@
+export class Types{
+  year: number;
+  title: string;
+  keyword: string;
+  something? : string;
+}
